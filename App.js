b159827923b8c1e545d94605d928721b19c35017
mahmoudrooa <mahmoudtarekmonaim@gmail.com>
2024-09-29 22:35:29 +0300

    first commit - initialize the porject node js

---
+++ App.js
@@ -0,0 +1,27 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/numbers", (req,res) => {
+
+	let numbers = ""
+	for(let i=0; i<=100;i++){
+		numbers += i + " _ ";
+	}
+
+res.send(`numbers:${numbers}`);
+});
+
+
+app.get("/mahmoud", (req,res) => {
+	res.sendfile(mahmoud"")
+});
+
+app.post("/addcomment",(req,res) => {
+	res.send("add comment")
+});
+
+app.listen(3000, () =>{
+
+	console.log("yes im listening in port 3000")
+});
